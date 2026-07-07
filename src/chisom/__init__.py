98a2f21abc077ce3d8eebd9d0b6dcb59b4f94ae6
Johannes Kaminski <j.kaminski@uni-muenster.de>
2026-07-07 17:04:36 +0200

    fix(gui): make start_som_view a lazy import so libary works on headless systems

--- src/chisom/__init__.py
+++ src/chisom/__init__.py
@@ -1,12 +1,22 @@
 from importlib.metadata import version
 
 from ._som import Som
-from ._interface.gui import start_chisom_viewer
+from .io.plotting import plot_som
 
 __all__ = [
     "Som",
+    "plot_som",
     "start_chisom_viewer",
 ]
+
+
+def __getattr__(name):
+    if name == "start_chisom_viewer":
+        from ._interface.gui import start_chisom_viewer
+
+        return start_chisom_viewer
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
 
 __version__ = version("chi-som")
 __author__ = "Johannes Kaminski"
--- src/chisom/io/__init__.py
+++ src/chisom/io/__init__.py
@@ -5,10 +5,12 @@ Classes and Functions for large on-disk data stores specific to cheminformatics
 from .datastore_creation import HDF5Creator
 from .datastore_factories import CSVStyleFactory, rdStyleFactory
 from .datastores import HDF5Dataset
+from .plotting import plot_som
 
 __all__ = [
     "HDF5Creator",
     "HDF5Dataset",
     "CSVStyleFactory",
     "rdStyleFactory",
+    "plot_som",
 ]
